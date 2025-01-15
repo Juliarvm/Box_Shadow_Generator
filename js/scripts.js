35f75996c5e36eaeb06b0f312aa3a8bb3d5e8fe7
juliaMelo <juliarvm04@gmail.com>
2025-01-15 14:48:39 -0300

    Adição de novas funcionalidades

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,44 @@ class Generator {
         this.mozRule = mozRule
     }
 
-    initialize()
-    {
+    initialize() {
         this.horizontalRef.value = this.horizontal.value;
         this.verticalRef.value = this.vertical.value;
         this.spreadRef.value = this.spread.value;
         this.blurRef.value = this.blur.value;
+        this.applyRule();
+        this.showRule();
+    }
+
+    applyRule() {
+        this.previewBox.style.boxShadow = `${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px #000000`
+        this.currentRule = this.previewBox.style.boxShadow;
+    }
+
+    showRule() {
+        this.rule.innerText = this.currentRule;
+        this.webkitRule.innerText = this.currentRule;
+        this.mozRule.innerText = this.currentRule;
+    }
+
+    updateValue(type, value) {
+        switch (type) {
+            case "horizontal":
+                this.horizontalRef.value = value;
+                break
+            case "vertical":
+                this.verticalRef.value = value;
+                break
+            case "blur":
+                this.blurRef.value = value;
+                break
+            case "spread":
+                this.spreadRef.value = value;
+                break
+
+        }
+        this.applyRule();
+        this.showRule();
     }
 }
 
@@ -43,5 +75,25 @@ const mozRule = document.querySelector("#moz-rule span")
 
 const boxShadow = new Generator(horizontal, horizontalRef, vertical, verticalRef, blur,
     blurRef, spread, spreadRef, previewBox, rule, webkitRule, mozRule);
-   boxShadow.initialize();
+boxShadow.initialize();
 //Eventos
+
+horizontal.addEventListener("input", (e) => {
+    const value = e.target.value;
+    boxShadow.updateValue("horizontal", value);
+})
+
+vertical.addEventListener("input", (e) => {
+    const value = e.target.value;
+    boxShadow.updateValue("vertical", value);
+})
+
+spread.addEventListener("input", (e) => {
+    const value = e.target.value;
+    boxShadow.updateValue("spread", value);
+})
+
+blur.addEventListener("input", (e) => {
+    const value = e.target.value;
+    boxShadow.updateValue("blur", value);
+})
