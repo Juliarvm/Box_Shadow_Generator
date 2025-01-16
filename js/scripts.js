eca3bcff0f0507c270963c038e34fced61cc217c
juliaMelo <juliarvm04@gmail.com>
2025-01-16 14:55:05 -0300

    Adição de novas funcionalidades

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 class Generator {
     constructor(horizontal, horizontalRef, vertical, verticalRef, blur,
-        blurRef, spread, spreadRef, color, colorRef, previewBox, rule, webkitRule, mozRule) {
+        blurRef, spread, spreadRef, color, colorRef, opacity, opacityRef,
+        previewBox, rule, webkitRule, mozRule, insetCheckbox) {
 
-        this.horizontal = horizontal
-        this.horizontalRef = horizontalRef
-        this.vertical = vertical
-        this.verticalRef = verticalRef
-        this.blur = blur
-        this.blurRef = blurRef
-        this.spread = spread
-        this.spreadRef = spreadRef
-        this.color = color
-        this.colorRef = colorRef
-        this.previewBox = previewBox
-        this.rule = rule
-        this.webkitRule = webkitRule
-        this.mozRule = mozRule
+        this.horizontal = horizontal;
+        this.horizontalRef = horizontalRef;
+        this.vertical = vertical;
+        this.verticalRef = verticalRef;
+        this.blur = blur;
+        this.blurRef = blurRef;
+        this.spread = spread;
+        this.spreadRef = spreadRef;
+        this.color = color;
+        this.colorRef = colorRef;
+        this.opacity = opacity;
+        this.opacityRef = opacityRef;
+        this.previewBox = previewBox;
+        this.rule = rule;
+        this.webkitRule = webkitRule;
+        this.mozRule = mozRule;
+        this.insetCheckbox = insetCheckbox;
     }
 
     initialize() {
@@ -24,12 +28,16 @@ class Generator {
         this.spreadRef.value = this.spread.value;
         this.colorRef.value = this.color.value;
         this.blurRef.value = this.blur.value;
+        this.opacityRef.value = this.opacity.value;
+        this.inset = this.insetCheckbox.checked ? 'inset' : '';
         this.applyRule();
         this.showRule();
     }
 
     applyRule() {
-        this.previewBox.style.boxShadow = `${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px${this.colorRef.value}`;
+        const rgbaColor = this.convertColorToRGBA(this.colorRef.value, this.opacityRef.value);
+        const insetValue = this.insetCheckbox.checked ? 'inset' : '';
+        this.previewBox.style.boxShadow = `${insetValue} ${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px ${rgbaColor}`;
         this.currentRule = this.previewBox.style.boxShadow;
     }
 
@@ -43,69 +51,132 @@ class Generator {
         switch (type) {
             case "horizontal":
                 this.horizontalRef.value = value;
-                break
+                this.horizontal.value = value; // Atualiza a barra de controle
+                break;
             case "vertical":
                 this.verticalRef.value = value;
-                break
+                this.vertical.value = value;
+                break;
             case "blur":
                 this.blurRef.value = value;
-                break
+                this.blur.value = value;
+                break;
             case "spread":
                 this.spreadRef.value = value;
-                break
+                this.spread.value = value;
+                break;
             case "color":
                 this.colorRef.value = value;
+                this.color.value = value;
                 break;
-
+            case "opacity":
+                this.opacityRef.value = value;
+                this.opacity.value = value;
+                break;
+            case "inset":
+                this.inset = value ? 'inset' : '';
+                break;
         }
         this.applyRule();
         this.showRule();
     }
+
+    convertColorToRGBA(hex, opacity) {
+        const r = parseInt(hex.substr(1, 2), 16);
+        const g = parseInt(hex.substr(3, 2), 16);
+        const b = parseInt(hex.substr(5, 2), 16);
+        return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+    }
 }
 
-//Seleção de elementos
-const horizontal = document.querySelector("#horizontal")
-const horizontalRef = document.querySelector("#horizontal-value")
-const vertical = document.querySelector("#vertical")
-const verticalRef = document.querySelector("#vertical-value")
-const blur = document.querySelector("#blur")
-const blurRef = document.querySelector("#blur-value")
-const spread = document.querySelector("#spread")
-const spreadRef = document.querySelector("#spread-value")
-const color = document.querySelector("#color")
-const colorRef = document.querySelector("#color-value")
+// Seleção de elementos
+const horizontal = document.querySelector("#horizontal");
+const horizontalRef = document.querySelector("#horizontal-value");
+const vertical = document.querySelector("#vertical");
+const verticalRef = document.querySelector("#vertical-value");
+const blur = document.querySelector("#blur");
+const blurRef = document.querySelector("#blur-value");
+const spread = document.querySelector("#spread");
+const spreadRef = document.querySelector("#spread-value");
+const color = document.querySelector("#color");
+const colorRef = document.querySelector("#color-value");
+const opacity = document.querySelector("#opacity");
+const opacityRef = document.querySelector("#opacity-value");
+const insetCheckbox = document.querySelector("#inset");
 
-const previewBox = document.querySelector("#box")
+const previewBox = document.querySelector("#box");
 
-const rule = document.querySelector("#rule span")
-const webkitRule = document.querySelector("#webkit-rule span")
-const mozRule = document.querySelector("#moz-rule span")
+const rule = document.querySelector("#rule span");
+const webkitRule = document.querySelector("#webkit-rule span");
+const mozRule = document.querySelector("#moz-rule span");
 
 const boxShadow = new Generator(horizontal, horizontalRef, vertical, verticalRef, blur,
-    blurRef, spread, spreadRef, color, colorRef, previewBox, rule, webkitRule, mozRule);
+    blurRef, spread, spreadRef, color, colorRef, opacity, opacityRef, previewBox, rule, webkitRule, mozRule, insetCheckbox);
 boxShadow.initialize();
-//Eventos
 
+// Eventos
 horizontal.addEventListener("input", (e) => {
     const value = e.target.value;
     boxShadow.updateValue("horizontal", value);
-})
+});
 
 vertical.addEventListener("input", (e) => {
     const value = e.target.value;
     boxShadow.updateValue("vertical", value);
-})
+});
 
 spread.addEventListener("input", (e) => {
     const value = e.target.value;
     boxShadow.updateValue("spread", value);
-})
+});
 
 blur.addEventListener("input", (e) => {
     const value = e.target.value;
     boxShadow.updateValue("blur", value);
-})
+});
+
 color.addEventListener("input", (e) => {
     const value = e.target.value;
     boxShadow.updateValue("color", value);
-})
+});
+
+opacity.addEventListener("input", (e) => {
+    const value = e.target.value;
+    boxShadow.updateValue("opacity", value);
+});
+
+insetCheckbox.addEventListener("change", (e) => {
+    const value = e.target.checked;
+    boxShadow.updateValue("inset", value);
+});
+
+// Atualizar valor das barras ao digitar no campo de texto
+horizontalRef.addEventListener("input", (e) => {
+    const value = e.target.value;
+    horizontal.value = value;  // Atualiza o slider
+    boxShadow.updateValue("horizontal", value);
+});
+
+verticalRef.addEventListener("input", (e) => {
+    const value = e.target.value;
+    vertical.value = value;
+    boxShadow.updateValue("vertical", value);
+});
+
+blurRef.addEventListener("input", (e) => {
+    const value = e.target.value;
+    blur.value = value;
+    boxShadow.updateValue("blur", value);
+});
+
+spreadRef.addEventListener("input", (e) => {
+    const value = e.target.value;
+    spread.value = value;
+    boxShadow.updateValue("spread", value);
+});
+
+opacityRef.addEventListener("input", (e) => {
+    const value = e.target.value;
+    opacity.value = value;
+    boxShadow.updateValue("opacity", value);
+});
